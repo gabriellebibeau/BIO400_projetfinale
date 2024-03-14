285c5c34c56692d2ec96ebb318065a7d03676a4b
gabriellebibeau <159631785+gabriellebibeau@users.noreply.github.com>
2024-03-14 15:12:13 -0400

    fonctionnel

--- fct_LV5.R
+++ fct_LV5.R
@@ -1,41 +1,44 @@
-#Fonction lotka voltera de base ----
-LV_base <- function(t, ConI, parmsf = c(r, v), parmsg = c(b,k), parmsLV = c(e,mu)){
+#Fonction lotka voltera avec IGP (5) ----
+LV5 <- function(t, ConI, parms5 = c(a,a_prime,alpha,b,b_prime,
+                                    beta,K,m,m_prime,r)){
   
-  with(as.list(ConI, parmsLV), {
+  with(as.list(ConI, parms5), {
     # Lotka-voltera
-    dN <- f(N, parmsf)*N - g(N,P, parmsg)*P # dN/dt
-    dP <- e*g(N,P, parmsg)*P - mu*P #dP/dt
+    dP <- P*(b_prime*a_prime*R + beta*alpha*N - m_prime) # dP/dt
+    dN <- N*(a*b*R - m - alpha*P) #dN/dt
+    dR <- R*(r*(1- (R/K)) - a*N - a_prime*P) #dR/dt
     
     # Resultat
-    res <- c(dN = dN, dP = dP)
+    res <- c(dP = dP, dN = dN, dR = dR)
     return(list(res))
   })
 }
 
-#Zone de test LV_base ----
-t <- 10 #aléatoire...
+#Zone de test LV5 ----
 
 #Conditions initiales arbitraires en proportion
-N0 <- 0.7 
-P0 <- 0.3
-Condition_Initiale <- c(N=N0, P=P0)
+P0 <- 15
+N0 <- 30
+R0 <- 100 - P0 - N0
+Condition_Initiale <- c(P=P0, N=N0, R=R0)
 
 #Paramètres selon la figure 1
-#f()
-r <- 1
-v <- 1
-parametres_f <- c(r=r,v=v)
+a         <- 1
+a_prime   <- 1 #entre 0 et 1 dans la fig1
+alpha     <- 0.5
+b         <- 1
+b_prime   <- 0.25 #entre 0 et 1 dans la fig1
+beta      <- 1
+K         <- 1
+m         <- 0.5
+m_prime   <- 0.5
+r         <- 1
 
-#g()
-b <- 2
-k <- 1
-parametres_g <- c(b=b,k=k)
-
-#LV_base()
-e <- 0.5
-mu <- 0.6
-parametres_LV <- c(e=e,mu=mu)
+para <- c(a=a,a_prime=a_prime,alpha=alpha,b=b,
+          b_prime=b_prime,beta=beta,K=K,m=m,
+          m_prime=m_prime,r=r)
 
 #test fct
-test_LV_base <- LV_base(t, Condition_Initiale, parametres_f, parametres_g, parametres_LV)
-#donne un nombre négatif pour dN, peut être flaw, à vérif éventuellement si la suite est erroné aussi
+test_LV5 <- LV5(5, Condition_Initiale, para);test_LV5
+#souvent nombres négatifs, mais j'ai l'impression que c'est juste un 
+#problème de paramètres... À garder en tête pour la suite
