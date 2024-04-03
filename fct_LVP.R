3126b5861426518cb09bdc726295b715530e5c6f
gabriellebibeau <bibeaugabrielle@gmail.com>
2024-04-03 13:31:52 -0400

    debut P2

---
+++ fct_LVP.R
@@ -0,0 +1,17 @@
+#Fonction lotka voltera avec IGP et prédateur suprême ----
+LV_SPNR <- function(t, ConI, parmsS = c(a_N,a_P,a_S,alpha,b_N,b_P,
+                                        b_S,beta,delta,K,lambda,m_N,
+                                        m_P,m_S,phi,psy,r)){
+  
+  with(as.list(ConI, parmsS), {
+    # Lotka-voltera
+    dS <- S*(b_S*a_S*R + phi*delta*N + psy*lambda*P- m_S) # dS/dt
+    dP <- P*(b_P*a_P*R + beta*alpha*N - lambda*S - m_P) # dP/dt
+    dN <- N*(a_N*b_N*R- alpha*P - delta*S - m_N) #dN/dt
+    dR <- R*(r*(1- (R/K)) - a_N*N - a_P*P - a_S*S) #dR/dt
+    
+    # Resultat
+    res <- c(dS = dS, dP = dP, dN = dN, dR = dR)
+    return(list(res))
+  })
+}
